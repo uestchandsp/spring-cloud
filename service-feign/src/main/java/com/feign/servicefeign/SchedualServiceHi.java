c0a5501147943520ce16c34b9e0820d973dccdda
梁鹏 <liangpeng@ruoshui.me>
2018-03-25 20:31:29 +0800

    init2

---
+++ service-feign/src/main/java/com/feign/servicefeign/SchedualServiceHi.java
@@ -0,0 +1,13 @@
+package com.feign.servicefeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+//定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
---
+++ service-ribbon/src/main/java/com/ribbon/serviceribbon/HelloService.java
@@ -0,0 +1,26 @@
+package com.ribbon.serviceribbon;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    //在hiService方法上加上@HystrixCommand注解。该注解对该方法创建了熔断器的功能，
+    // 并指定了fallbackMethod熔断方法
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
+}
